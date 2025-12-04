54407232c8f0d59924fd2b7fd84f4699917db0a8
Atticus Walton <139408126+ExcessMarrow773@users.noreply.github.com>
2025-12-04 14:56:04 -0600

    save

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
+
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-#@onready var death_plane: Area2D = $"../Death Plane"
 @onready var player: CharacterBody2D = $"../Player"
 @onready var world: Node2D = get_tree().root.get_node("World")
 @onready var navigation_agent = $NavigationAgent2D
+@onready var scripts = "res://Scripts/contrib.gd"
 
 @export var MAX_SPEED := 150
 @export var JUMP_VELOCITY := -350
@@ -25,34 +26,27 @@ func _ready() -> void:
 	
 
 func flip(direction):
-	if direction == -1:
+	if direction.x < 0:
 		return true
 	else:
 		return false
 
 func _physics_process(delta: float) -> void:
 	navigation_agent.target_position = player.position
+	var next_path_position = navigation_agent.get_next_path_position()
+	var direction := global_position.direction_to(next_path_position)
+	
 	if world.enable_enemy_pathfinding:
-		var next_path_position = navigation_agent.get_next_path_position()
-		var direction := global_position.direction_to(next_path_position)
-		
-
 		velocity += get_gravity() * delta
 		move_and_slide()
 		print(direction)
-		# Handles respawn/ restart
-		if Input.is_action_just_pressed("restart"):
-			velocity = Vector2(0, 0)
-			restart()
-
-		# Get the input direction and handle the movement/deceleration.
-		if not game_paused: # Adjust the threshold (0.1) as needed:
+		
+		if not game_paused:
+			velocity.x = (direction.x * SPEED)
 			
-			velocity = direction * SPEED
-			
-			if not game_paused:
-				anim.pause()
-				anim.play("Walk")
+			anim.pause()
+			anim.play("Walk")
+			$AnimatedSprite2D.flip_h = flip(direction)
 		
 		else:
 			if not game_paused:
@@ -61,16 +55,16 @@ func _physics_process(delta: float) -> void:
 
 		move_and_slide()
 		
+		
+		## KEYBINDS
+		if Input.is_action_just_pressed("restart"):
+			velocity = Vector2(0, 0)
+			scripts.restart()
+			
 		if Input.is_action_just_pressed("quit"): get_tree().quit()
+		
 	else:
 		# Stop enemy movement if pathfinding is disabled
 		velocity = Vector2.ZERO
 		anim.pause()
 		anim.play("Idle")
-	
-func restart():
-	get_tree().reload_current_scene()
-
-func _on_animated_sprite_2d_animation_finished(anim_name) -> void:
-	print("finished animation")
-	animation_finished = anim_name
